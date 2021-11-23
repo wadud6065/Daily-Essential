610381f4290770dbaa775f84ad260d7d40c20cd9
Toufiq2018331084 <52928065+Toufiq2018331084@users.noreply.github.com>
2021-11-24 01:53:25 +0600

    calendar added

---
+++ app/src/main/java/com/example/dailyessential/dailyroutine/CalendarActivity.java
@@ -0,0 +1,30 @@
+package com.example.dailyessential.dailyroutine;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+import com.example.dailyessential.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        calendarView = (CalendarView) findViewById(R.id.idcalendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "onSelectedDayChange: " + dayOfMonth + month + year);
+            }
+        });
+    }
+}
