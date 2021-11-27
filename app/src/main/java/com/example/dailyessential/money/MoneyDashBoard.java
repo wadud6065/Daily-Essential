c73efb3f638b791c96d10e221cfca168cf1440dd
wadud6065 <wadud6065@gmail.com>
2021-11-27 21:06:25 +0600

    Fragment Initialization Fixed

--- app/src/main/java/com/example/dailyessential/money/MoneyDashBoard.java
+++ app/src/main/java/com/example/dailyessential/money/MoneyDashBoard.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -24,9 +26,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.dailyessential.DashBoard;
 import com.example.dailyessential.R;
 import com.example.dailyessential.money.model.Data;
 import com.example.dailyessential.money.model.TodayItemsAdapter;
+import com.example.dailyessential.money.view.Today_Fragment;
+import com.example.dailyessential.notes.CreateNoteActivity;
+import com.example.dailyessential.notes.MainNoteActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -82,36 +88,22 @@ public class MoneyDashBoard extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
         nav = findViewById(R.id.navId);
-        nav.getMenu().getItem(0).setChecked(true);
         nav.setItemIconTintList(null);
         drawerLayout = findViewById(R.id.drawerLayoutId);
 
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open,R.string.close);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        totalAmountSpentOn = findViewById(R.id.totalAmountSpentOn);
-        progressBar = findViewById(R.id.progressBar);
-
-        fab = findViewById(R.id.fab);
-        loader = new ProgressDialog(this);
-
-        mAuth = FirebaseAuth.getInstance();
-        onlineUserId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
-        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setStackFromEnd(true);
-        linearLayoutManager.setReverseLayout(true);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        myDataList = new ArrayList<>();
-        readItems();
-
-        todayItemsAdapter  = new TodayItemsAdapter(MoneyDashBoard.this, myDataList);
-        recyclerView.setAdapter(todayItemsAdapter);
+        if(savedInstanceState == null) {
+            Today_Fragment todayFragment = new Today_Fragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, todayFragment, "findThisFragment")
+                    .addToBackStack(null)
+                    .commit();
+            nav.setCheckedItem(R.id.today);
+        }
 
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @SuppressLint("NonConstantResourceId")
@@ -119,148 +111,48 @@ public class MoneyDashBoard extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()) {
+                    case R.id.today:
+                        Toast.makeText(MoneyDashBoard.this, "Yes", Toast.LENGTH_LONG).show();
+                        Today_Fragment todayFragment = new Today_Fragment();
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, todayFragment, "findThisFragment")
+                                .addToBackStack(null)
+                                .commit();
+                        drawerLayout.closeDrawers();
+                        break;
+
                     case R.id.mybudget:
+                        break;
+
+                    case R.id.this_week:
+                        break;
+
+                    case R.id.this_month:
+                        break;
+
+                    case R.id.history:
+                        break;
+
+                    case R.id.today_analytics:
+                        break;
+
+                    case R.id.week_analytics:
+                        break;
+
+                    case R.id.month_analytics:
                         break;
                 }
 
                 return true;
             }
         });
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItemSpentOn();
-            }
-        });
     }
-
-    private void readItems() {
-
-        @SuppressLint("SimpleDateFormat")
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        Calendar cal = Calendar.getInstance();
-        String date = dateFormat.format(cal.getTime());
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
-        Query query = reference.orderByChild("date").equalTo(date);
-        query.addValueEventListener(new ValueEventListener() {
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                myDataList.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    Data data = dataSnapshot.getValue(Data.class);
-                    myDataList.add(data);
-                }
-
-                todayItemsAdapter.notifyDataSetChanged();
-                progressBar.setVisibility(View.GONE);
-
-                int totalAmount = 0;
-                for (DataSnapshot ds: snapshot.getChildren()){
-                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
-                    Object total = map.get("amount");
-                    int pTotal = Integer.parseInt(String.valueOf(total));
-                    totalAmount += pTotal;
-
-                    totalAmountSpentOn.setText("Total Day's Spending: $"+totalAmount);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-
-    }
-
-    private void addItemSpentOn() {
-        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View myView = inflater.inflate(R.layout.input_layout, null);
-        myDialog.setView(myView);
-
-        final  AlertDialog dialog = myDialog.create();
-        dialog.setCancelable(false);
-
-        final Spinner itemSpinner = myView.findViewById(R.id.itemsspinner);
-        final EditText amount = myView.findViewById(R.id.amount);
-        final EditText note = myView.findViewById(R.id.note);
-        final Button cancel = myView.findViewById(R.id.cancel);
-        final  Button save = myView.findViewById(R.id.save);
-
-        note.setVisibility(View.VISIBLE);
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String Amount = amount.getText().toString();
-                String Item = itemSpinner.getSelectedItem().toString();
-                String notes = note.getText().toString();
-
-                if (TextUtils.isEmpty(Amount)){
-                    amount.setError("Amount is required!");
-                    return;
-                }
-
-                if (Item.equals("Select item")){
-                    Toast.makeText(MoneyDashBoard.this, "Select a valid item", Toast.LENGTH_SHORT).show();
-                }
-
-                if (TextUtils.isEmpty(notes)){
-                    note.setError("Note is required");
-                    return;
-                }
-
-                else {
-                    loader.setMessage("adding a budget item");
-                    loader.setCanceledOnTouchOutside(false);
-                    loader.show();
-
-                    String id  = expensesRef.push().getKey();
-                    @SuppressLint("SimpleDateFormat")
-                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                    Calendar cal = Calendar.getInstance();
-                    String date = dateFormat.format(cal.getTime());
-
-                    MutableDateTime epoch = new MutableDateTime();
-                    epoch.setDate(0);
-                    DateTime now = new DateTime();
-                    Weeks weeks = Weeks.weeksBetween(epoch, now);
-                    Months months = Months.monthsBetween(epoch, now);
-
-                    String itemNday = Item+date;
-                    String itemNweek = Item+weeks.getWeeks();
-                    String itemNmonth = Item+months.getMonths();
-
-                    Data data = new Data(Item, date, id, itemNday, itemNweek, itemNmonth, Integer.parseInt(Amount), weeks.getWeeks(), months.getMonths(), notes);
-                    expensesRef.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()){
-                                Toast.makeText(MoneyDashBoard.this, "Budget item added successfuly", Toast.LENGTH_SHORT).show();
-                            }else {
-                                Toast.makeText(MoneyDashBoard.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
-                            }
-
-                            loader.dismiss();
-                        }
-                    });
-                    dialog.dismiss();
-                }
-            }
-        });
-
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-        dialog.show();
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
